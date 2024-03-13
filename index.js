f9b542e73051aa64e8cb4deafbc84c8efec8bd3d
Korneel Eeckhout <korneel@pienter.tech>
2024-03-13 16:48:17 +0100

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { obj } = require("through2");
-const { heml } = require("@dragonzap/heml");
-const path = require("path");
+import { obj } from "through2";
+import heml from "@dragonzap/heml";
+import path from "path";
 var ext = ".html";
 
-exports.default = (options) => {
+export default (options) => {
   return obj((file, enc, cb) => {
     console.log("Gulp-HEML: starting to process file: " + file.path);
 
@@ -17,26 +17,27 @@ exports.default = (options) => {
       return cb();
     }
 
-    return heml(file.contents.toString(), options)
-      .then((hemlResp) => {
-        console.log("Gulp-HEML: starting to process file: " + file.path);
-        file.contents = Buffer.from(hemlResp.html);
-        var replaceExt = replaceExt || false;
-        if (typeof ext === "string" && ext.length > 0) {
-          ext = ext.indexOf(".") === 0 ? ext : "." + ext;
-          let filePath = path.parse(file.path);
-          filePath.base = filePath.base.replace(
-            replaceExt ? replaceExt : path.extname(file.path),
-            ext,
-          );
-          // format the path back into an absolute
-          file.path = path.format(filePath);
-        }
+    return heml(file.contents.toString(), options).then((hemlResp) => {
+      console.log("Gulp-HEML: starting to process file: " + file.path);
+      file.contents = Buffer.from(hemlResp.html);
+      var replaceExt = replaceExt || false;
+      if (typeof ext === "string" && ext.length > 0) {
+        ext = ext.indexOf(".") === 0 ? ext : "." + ext;
+        let filePath = path.parse(file.path);
+        filePath.base = filePath.base.replace(
+          replaceExt ? replaceExt : path.extname(file.path),
+          ext,
+        );
+        // format the path back into an absolute
+        file.path = path.format(filePath);
+      }
 
-        console.log("Gulp-HEML: finished processing file: " + file.path);
+      console.log("Gulp-HEML: finished processing file: " + file.path);
 
-        this.push(file);
-      })
-      .then(() => cb());
+      this.push(file);
+      return cb();
+    });
   });
 };
+
+export { heml };
